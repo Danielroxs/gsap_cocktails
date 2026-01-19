46bac57614603127124e027aa696085b0b6af9c9
Danielroxs <danroxsll@gmail.com>
2026-01-19 13:00:33 -0600

    feat: add Navbar section

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import gsap from "gsap";
 import { SplitText } from "gsap/SplitText";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import React from "react";
+import Navbar from "./components/Navbar";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
   return (
-    <div className="flex-center h-screen">
-      <h1 className="text-3xl text-indigo-300">Hola GSAP!</h1>
-    </div>
+    <main>
+      <Navbar />
+    </main>
   );
 };
 
